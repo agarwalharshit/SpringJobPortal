ab19968f73ee16aac70199931cb11d2cfb24a9a2
Harshit Agarwal <Harshit@harshit.local>
2017-05-14 11:48:34 -0700

    removing

--- Lab2.data/src/main/java/edu/sjsu/cmpe275/user/UserProfileRepository.java
+++
@@ -1,9 +0,0 @@
-package edu.sjsu.cmpe275.user;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserProfileRepository  extends CrudRepository<UserProfile,Integer>{
-
-	//Passenger save(Passenger passenger, int id);
-
-}
